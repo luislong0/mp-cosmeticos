2a9eb09eee66de3f1ac7b48ccd1327e87b63420f
luislong0 <luisptavio.l.p@gmail.com>
2024-04-30 17:33:09 -0300

    feat: add more products and fix catalog

--- src/app/api/products/route.ts
+++
@@ -1,7 +0,0 @@
-import data from './data.json'
-
-export async function GET() {
-  return new Response(JSON.stringify(data.products), {
-    headers: { 'Content-Type': 'application/json' },
-  })
-}
